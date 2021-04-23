b91f4515fb7d6c218cc6e83080d40da84e39f9db
Gonzalo <errandoneagonzalo18@gmail.com>
2021-04-23 10:59:23 -0300

    cambie el algoritmo del laberinto

--- laberinto.py
+++ laberinto.py
@@ -4,7 +4,8 @@ Matriz = [[1,1,0,1,0],
           [1,1,0,1,0],
           [1,0,1,1,1]]
 
-def Buscar_Salida(Laberinto, x, y):
+#algoritmo viejo
+"""def Buscar_Salida(Laberinto, x, y):
     
     if(x == 4 and y == 4):
         return 1
@@ -13,25 +14,19 @@ def Buscar_Salida(Laberinto, x, y):
         Buscar_Salida(Laberinto, x+1, y)
         Buscar_Salida(Laberinto, x-1, y)
         Buscar_Salida(Laberinto, x, y-1)
-    
+"""
 
-print(Buscar_Salida(Matriz, 0, 0))
-
-"""def Buscar_Salida(Laberinto,x,y):
-    if x == 4 and y == 5:
-        return Laberinto
+def Buscar_Salida(Laberinto, x, y, tamanio_matriz):
+    if x == tamanio_matriz and y == tamanio_matriz:
+        Laberinto[x][y] = 2
+        return True
     else:
         if (((x >= 0 and x <= 4) and (y >= 0 and y <= 4)) and (Laberinto[x][y] == 1)):
-            Laberinto[x][y]=2
-            if Buscar_Salida(Laberinto,x,y+1):
-                return Laberinto
-            elif Buscar_Salida(Laberinto,x+1,y):
-                return Laberinto
-            elif Buscar_Salida(Laberinto,x-1,y):
-                return Laberinto
-            elif Buscar_Salida(Laberinto,x,y-1):
-                return Laberinto
+            Laberinto[x][y] = 2
+            if Buscar_Salida(Laberinto,x,y+1, tamanio_matriz) or Buscar_Salida(Laberinto,x+1,y, tamanio_matriz) or Buscar_Salida(Laberinto,x-1,y, tamanio_matriz) or Buscar_Salida(Laberinto,x,y-1, tamanio_matriz):
+                return True
         else:
             return False
 
-print(Buscar_Salida(Matriz,0,0))"""
+print(Buscar_Salida(Matriz, 0, 0, (len(Matriz)-1)))
+print(Matriz)
