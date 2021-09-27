a685771152b8ba57f3a3d7460d7c85162af1380a
errandoneagonzalo18@gmail.com <Gonzalo>
2021-09-27 10:01:38 -0300

    TP 5 al 90

--- TP 5/ejercicio24.py
+++ TP 5/ejercicio24.py
@@ -21,7 +21,7 @@ def contar_ocurrencias_de_heroe(arbol, nombre_heroe):
     contador = 0
 
     if(arbol.info is not None):
-        if(arbol.datos['Derrotado'] == nombre_heroe):
+        if(arbol.datos['Capturado'] == nombre_heroe):
             contador += 1
         if(arbol.izq is not None):
             contador += contar_ocurrencias_de_heroe(arbol.izq, nombre_heroe)
@@ -43,7 +43,7 @@ def recompilar_datos_de_un_arbol(arbol, campo, vec_datos=[]):
     return vec_datos
 
 def contar_mayor_cantidad_de_criaturas_derrotadas_por_un_heroe(arbol):
-    vector_heroes = recompilar_datos_de_un_arbol(arbol, 'Derrotado')
+    vector_heroes = recompilar_datos_de_un_arbol(arbol, 'Capturado')
     ocurrencias = 0
     lista_dic_heroes = Lista()
 
@@ -51,8 +51,8 @@ def contar_mayor_cantidad_de_criaturas_derrotadas_por_un_heroe(arbol):
         ocurrencias = contar_ocurrencias_de_heroe(arbol, 
                         nombre_heroe)
         lista_dic_heroes.insertar({'nombre': nombre_heroe,
-                                 'cantidad_derrotados': ocurrencias}, 
-                                 'cantidad_derrotados')
+                                 'cantidad_Capturados': ocurrencias}, 
+                                 'cantidad_Capturados')
     
     return lista_dic_heroes
 
@@ -67,75 +67,143 @@ def inorden_criaturas_derrotadas_por_un_heroe(arbol, nombre):
     if(arbol.info is not None):
         if(arbol.izq is not None):
             inorden_criaturas_derrotadas_por_un_heroe(arbol.izq, nombre)
-        if(arbol.datos['Derrotado'] == nombre):
+        if(arbol.datos['Capturado'] == nombre):
             print(arbol.datos['Criatura'])
         if(arbol.der is not None):
             inorden_criaturas_derrotadas_por_un_heroe(arbol.der, nombre)
     
+#PUNTO H
+def modificar_campo_capturado_de_una_criatura(arbol, criaturas, capturado_por):
 
+    for criatura in criaturas:
+        pos = arbol.busqueda(criatura)
+        if(pos is not None):
+            pos.datos['Capturado'] = capturado_por
+        else:
+            print('La criatura "' + criatura + '" no esta en el arbol')
+
+
+#PUNTO J
+def eliminar_criatura_del_arbol(arbol, criaturas):
+
+    for criatura in criaturas:
+        info, datos = arbol.eliminar_nodo(criatura)
+
+        if(info is None or datos is None):
+            print('La criatura "' + criatura + '" no se encontro en el arbol')
+        else:
+            print('Eliminado: ', info, '\n', datos)
+
+#PUNTO L
+def modificar_el_nombre_de_una_criatura(arbol, criatura, modificarlo_por):
+    pos = arbol.busqueda(criatura)
+    if(pos is not None):
+        pos.datos['Criatura'] = modificarlo_por
+        print('\nNombre de la criatura modificada con exito', '\n', pos.datos)
+    else:
+        print('La criatura "' + criatura + '" no se encontro en el arbol')
 
 def ejercicio_24():
     lista_dic_heroes = Lista()
-    dic_criaturas = [{'Criatura': 'Ceto', 'Derrotado': '-', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Tifon', 'Derrotado': 'Zeus', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Equidna', 'Derrotado': 'Argos panoptnes', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Dino', 'Derrotado': '-', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Pefredo', 'Derrotado': '-', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Enio', 'Derrotado': '-', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Escila', 'Derrotado': '-', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Caribdis', 'Derrotado': '-', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Euriale', 'Derrotado': '-', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Esteno', 'Derrotado': '-', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Medusa', 'Derrotado': 'Perseo', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Ladon', 'Derrotado': 'Heracles', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Aguila del Caucaso', 'Derrotado': '-', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Quimera', 'Derrotado': 'Belerofonte', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Hidra de Lerna', 'Derrotado': 'Heracles', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Leon de Nemea', 'Derrotado': 'Heracles', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Esfinge', 'Derrotado': 'Edipo', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Dragon de la Colquida', 'Derrotado': '-', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Gerbero', 'Derrotado': '-', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Cerda de Cromion', 'Derrotado': 'Teseo', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Ortro', 'Derrotado': 'Heracles', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Toro de Creta', 'Derrotado': 'Teseo', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Jabali De Calidon', 'Derrotado': 'Atalanta', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Carcinos', 'Derrotado': '-', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Gerion', 'Derrotado': 'Heracles', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Cloto', 'Derrotado': '-', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Laquesis', 'Derrotado': '-', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Atropos', 'Derrotado': '-', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Minotauro de Creta', 'Derrotado': 'Teseo', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Harpias', 'Derrotado': '-', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Argos Panoptnes', 'Derrotado': 'Hermes', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Aves del Estinfalo', 'Derrotado': '-', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Talos', 'Derrotado': 'Medea', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Sirenas', 'Derrotado': '-', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Piton', 'Derrotado': 'Apolo', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Cierva de Cerinea', 'Derrotado': '-', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Basilisco', 'Derrotado': '-', 'Descripcion' : 'No hay descripcion'},
-                     {'Criatura': 'Jabali del Erimanto', 'Derrotado': '-', 'Descripcion' : 'No hay descripcion'},
-
-                    ]
     arbol_criaturas = Arbol()
-
+    dic_criaturas = [{'Criatura': 'Ceto', 'Capturado': '-', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Tifon', 'Capturado': 'Zeus', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Equidna', 'Capturado': 'Argos panoptnes', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Dino', 'Capturado': '-', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Pefredo', 'Capturado': '-', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Enio', 'Capturado': '-', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Escila', 'Capturado': '-', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Caribdis', 'Capturado': '-', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Euriale', 'Capturado': '-', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Esteno', 'Capturado': '-', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Medusa', 'Capturado': 'Perseo', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Ladon', 'Capturado': 'Heracles', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Aguila del Caucaso', 'Capturado': '-', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Quimera', 'Capturado': 'Belerofonte', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Hidra de Lerna', 'Capturado': 'Heracles', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Leon de Nemea', 'Capturado': 'Heracles', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Esfinge', 'Capturado': 'Edipo', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Dragon de la Colquida', 'Capturado': '-', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Cerbero', 'Capturado': '-', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Cerda de Cromion', 'Capturado': 'Teseo', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Ortro', 'Capturado': 'Heracles', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Toro de Creta', 'Capturado': 'Teseo', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Jabali De Calidon', 'Capturado': 'Atalanta', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Carcinos', 'Capturado': '-', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Gerion', 'Capturado': 'Heracles', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Cloto', 'Capturado': '-', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Laquesis', 'Capturado': '-', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Atropos', 'Capturado': '-', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Minotauro de Creta', 'Capturado': 'Teseo', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Harpias', 'Capturado': '-', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Argos Panoptnes', 'Capturado': 'Hermes', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Aves del Estinfalo', 'Capturado': '-', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Talos', 'Capturado': 'Medea', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Sirenas', 'Capturado': '-', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Piton', 'Capturado': 'Apolo', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Cierva de Cerinea', 'Capturado': '-', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Basilisco', 'Capturado': '-', 'Descripcion' : 'No hay descripcion'},
+                     {'Criatura': 'Jabali de Erimanto', 'Capturado': '-', 'Descripcion' : 'No hay descripcion'},
+                    ]
+    
     arbol_criaturas = arbol_criaturas.cargar_arbol(dic_criaturas, 'Criatura')
 
+    #PUNTO A
     print('\nLISTADO INORDEN DE LAS CRIATURAS')
-    #arbol_criaturas.inorden()
+    # arbol_criaturas.inorden()
 
+    #PUNTO B
     print('\nCARGAR DESCRIPCION DE CADA CRIATURA')
-    #cargar_descripcion(arbol_criaturas)
+    # cargar_descripcion(arbol_criaturas)
 
+    #PUNTO C
     print('\nMOSTRAR LA INFORMACION DE TALOS')
-    #print(arbol_criaturas.busqueda('Talos').datos)
+    # print(arbol_criaturas.busqueda('Talos').datos)
 
+    #PUNTO D
     print('\nDETERMINAR LOS 3 HEROES O DIOSES'
             + 'QUE DERROTARON MAYOR CANTIDAD DE CRIATURAS')
     # lista_dic_heroes = contar_mayor_cantidad_de_criaturas_derrotadas_por_un_heroe(arbol_criaturas)
     # imprimir_tres_heroes_con_mas_criaturas_derrotadas(lista_dic_heroes)
 
+    #PUNTO E
     print('\nIMPRIMIR LAS CRIATURAS DERROTADAS POR HERACLES')
-    inorden_criaturas_derrotadas_por_un_heroe(arbol_criaturas, 'Heracles')
+    # inorden_criaturas_derrotadas_por_un_heroe(arbol_criaturas, 'Heracles')
+
+    #PUNTO F
+    print('\nIMPRIMIR LAS CRIATURAS DERROTADAS QUE NO HAN SIDO DERROTADAS')
+    # inorden_criaturas_derrotadas_por_un_heroe(arbol_criaturas, '-')
+
+    #PUNTO H
+    print('\nMODIFICAR LOS NODOS CERBERO, TORO DE CRETA, CIERVA CERINEA,'
+          + ' Y JABALI DE ERIMANTO, E INDICAR QUE HERACLES LAS ATRAPO')
+    # vec_criaturas = ['Cerbero', 'Toro de Creta', 'Cierva de Cerinea', 
+    #                 'Jabali de Erimanto']
+    # modificar_campo_capturado_de_una_criatura(arbol_criaturas, 
+    #                                             vec_criaturas,'Heracles')
+
+    #PUNTO I
+    print('\nBUSQUEDA POR PROXIMIDAD')
+    # buscar = input('Ingrese el nombre de la criatura a buscar: ')
+    # arbol_criaturas.busqueda_proximidad(buscar)
+
+    #PUNTO J
+    print('\nELIMINAR AL BASILISCO Y A LAS SIRENAS')
+    # vec_criaturas_a_eliminar = ['Basilisco', 'Las Sirenas']
+    # eliminar_criatura_del_arbol(arbol_criaturas, vec_criaturas_a_eliminar)
+
+    #PUNTO K
+
+
+    #PUNTO L
+    print('\nCAMBIAR EL NOMBRE DE LADON POR DRAGON LADON')
+    modificar_el_nombre_de_una_criatura(arbol_criaturas, 'Ladon', 'Dragon Ladon')
+
+    # #PUNTO M
+    print('\nLISTADO POR NIVEL DEL ARBOL')
+    arbol_criaturas.barrido_por_nivel()
+
+    #PUNTO N
 
 
 ejercicio_24()
